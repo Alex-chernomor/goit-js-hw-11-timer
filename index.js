dc7a101063aa3a5a6dda6352fdbbd7f9a933f565
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2020-10-29 20:34:30 +0200

    1
    1

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-function getTimeRemaining(endtime) {
-  let t = Date.parse(endtime) - Date.parse(new Date());
-  let seconds = Math.floor((t / 1000) % 60);
-  let minutes = Math.floor((t / 1000 / 60) % 60);
-  let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  let days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
+
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = (new Date(targetDate));
+
+  }
+  timer() {
+    let timir = document.querySelector(this.selector)
+    let daysSpan = timir.querySelector('.days');
+    let hoursSpan = timir.querySelector('.hours');
+    let minutesSpan = timir.querySelector('.minutes');
+    let secondsSpan = timir.querySelector('.seconds');
+    
+    
+    setInterval(()=>{
+      const startTime = Date.now();
+      const deltaTime = this.targetDate-startTime ; 
+      const days = pad(Math.floor((deltaTime / (1000 * 60 * 60 * 24))));
+      const hours = pad(Math.floor((deltaTime % (1000*60*60*24))/(1000*60*60)));
+      const mins = pad(Math.floor((deltaTime % (1000*60*60))/(1000*60)));
+      const secs = pad(Math.floor((deltaTime % (1000*60))/1000));
+      
+      daysSpan.textContent = days;
+      hoursSpan.textContent = hours;
+      minutesSpan.textContent = mins;
+      secondsSpan.textContent = secs;
+    },1000);
+    
   };
 }
- 
-function initializeClock(id, endtime) {
-  let clock = document.getElementById(id);
-  let daysSpan = clock.querySelector('.days');
-  let hoursSpan = clock.querySelector('.hours');
-  let minutesSpan = clock.querySelector('.minutes');
-  let secondsSpan = clock.querySelector('.seconds');
- 
-  function updateClock() {
-    let t = getTimeRemaining(endtime);
- 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
- 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
- 
-  updateClock();
-  let timeinterval = setInterval(updateClock, 1000);
+const time2 = new CountdownTimer('#countdown', '2022, 11, 28');
+const time3 = new CountdownTimer('#countdown2', '2020, 10, 30');
+
+
+time2.timer();
+time3.timer();
+
+function pad(value){
+return String(value).padStart(2,'0')
 }
 
- let date = new Date(2020, 10, 16);
 
- let deltaDate = date-Date.now(); 
 
- let deadline = new Date(Date.parse(new Date()) + deltaDate); // for endless timer
-initializeClock('countdown', deadline);
